a05fd34d3e30945b55b1592e058c87407b44edd0
SnehaThapa16 <thapasneha444@gmail.com>
2025-06-27 14:18:35 +0530

    Fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ IMPORTANT: Add this line to fix Netlify white screen issue
 })
